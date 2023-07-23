553f7bc79562f2a65cd19ed6c49d20750d71f596
ddeoyoung <ksysnake3690@gmail.com>
2023-07-23 23:25:40 +0900

    엔진 업데이트

--- DirectX_KSY/Content_Cuphead/ContentsCore.cpp
+++ DirectX_KSY/Content_Cuphead/ContentsCore.cpp
@@ -67,28 +67,28 @@ void ContentsCore::Update(float _Delta)
 		Vertex[6] = float4::VectorRotationToDegX(Vertex[2], 180.0f);
 		Vertex[7] = float4::VectorRotationToDegX(Vertex[3], 180.0f);
 
-		//// 왼쪽이나 오른쪽
-		//Vertex[8] = float4::VectorRotationToDegY(Vertex[0], 90.0f);
-		//Vertex[9] = float4::VectorRotationToDegY(Vertex[1], 90.0f);
-		//Vertex[10] = float4::VectorRotationToDegY(Vertex[2], 90.0f);
-		//Vertex[11] = float4::VectorRotationToDegY(Vertex[3], 90.0f);
+		// 왼쪽이나 오른쪽
+		Vertex[8] = float4::VectorRotationToDegY(Vertex[0], 90.0f);
+		Vertex[9] = float4::VectorRotationToDegY(Vertex[1], 90.0f);
+		Vertex[10] = float4::VectorRotationToDegY(Vertex[2], 90.0f);
+		Vertex[11] = float4::VectorRotationToDegY(Vertex[3], 90.0f);
 
-		//// 왼쪽이나 오른쪽
-		//Vertex[12] = float4::VectorRotationToDegY(Vertex[0], -90.0f);
-		//Vertex[13] = float4::VectorRotationToDegY(Vertex[1], -90.0f);
-		//Vertex[14] = float4::VectorRotationToDegY(Vertex[2], -90.0f);
-		//Vertex[15] = float4::VectorRotationToDegY(Vertex[3], -90.0f);
+		// 왼쪽이나 오른쪽
+		Vertex[12] = float4::VectorRotationToDegY(Vertex[0], -90.0f);
+		Vertex[13] = float4::VectorRotationToDegY(Vertex[1], -90.0f);
+		Vertex[14] = float4::VectorRotationToDegY(Vertex[2], -90.0f);
+		Vertex[15] = float4::VectorRotationToDegY(Vertex[3], -90.0f);
 
-		//// 위거나 아래
-		//Vertex[16] = float4::VectorRotationToDegX(Vertex[0], 90.0f);
-		//Vertex[17] = float4::VectorRotationToDegX(Vertex[1], 90.0f);
-		//Vertex[18] = float4::VectorRotationToDegX(Vertex[2], 90.0f);
-		//Vertex[19] = float4::VectorRotationToDegX(Vertex[3], 90.0f);
+		// 위거나 아래
+		Vertex[16] = float4::VectorRotationToDegX(Vertex[0], 90.0f);
+		Vertex[17] = float4::VectorRotationToDegX(Vertex[1], 90.0f);
+		Vertex[18] = float4::VectorRotationToDegX(Vertex[2], 90.0f);
+		Vertex[19] = float4::VectorRotationToDegX(Vertex[3], 90.0f);
 
-		//Vertex[20] = float4::VectorRotationToDegX(Vertex[0], -90.0f);
-		//Vertex[21] = float4::VectorRotationToDegX(Vertex[1], -90.0f);
-		//Vertex[22] = float4::VectorRotationToDegX(Vertex[2], -90.0f);
-		//Vertex[23] = float4::VectorRotationToDegX(Vertex[3], -90.0f);
+		Vertex[20] = float4::VectorRotationToDegX(Vertex[0], -90.0f);
+		Vertex[21] = float4::VectorRotationToDegX(Vertex[1], -90.0f);
+		Vertex[22] = float4::VectorRotationToDegX(Vertex[2], -90.0f);
+		Vertex[23] = float4::VectorRotationToDegX(Vertex[3], -90.0f);
 
 
 
@@ -98,14 +98,14 @@ void ContentsCore::Update(float _Delta)
 			0, 2, 3,
 			0 + 4, 1 + 4, 2 + 4,
 			0 + 4, 2 + 4, 3 + 4,
-			//0 + 8, 1 + 8, 2 + 8,
-			//0 + 8, 2 + 8, 3 + 8,
-			//0 + 12, 1 + 12, 2 + 12,
-			//0 + 12, 2 + 12, 3 + 12,
-			//0 + 16, 1 + 16, 2 + 16,
-			//0 + 16, 2 + 16, 3 + 16,
-			//0 + 20, 1 + 20, 2 + 20,
-			//0 + 20, 2 + 20, 3 + 20,
+			0 + 8, 1 + 8, 2 + 8,
+			0 + 8, 2 + 8, 3 + 8,
+			0 + 12, 1 + 12, 2 + 12,
+			0 + 12, 2 + 12, 3 + 12,
+			0 + 16, 1 + 16, 2 + 16,
+			0 + 16, 2 + 16, 3 + 16,
+			0 + 20, 1 + 20, 2 + 20,
+			0 + 20, 2 + 20, 3 + 20,
 		};
 
 		// 0   1
@@ -136,23 +136,32 @@ void ContentsCore::Update(float _Delta)
 			ArrIndex[1] = Index[indexCount * 3 + 1];
 			ArrIndex[2] = Index[indexCount * 3 + 2];
 
+			float4 Trifloat4[3];
 			std::vector<POINT> Tri;
 			Tri.resize(3);
 			for (size_t VertexCount = 0; VertexCount < Tri.size(); VertexCount++)
 			{
-				// 위치를 더해줌으로해서 월드 상태로 이전시켰다고 한다.
 				float4 WorldPoint = Vertex[ArrIndex[VertexCount]];
-
-				// 위치 크기 회전을 적용시킬때 수학적으로 증명된
-				// 절대적인 기준이 있습니다.
-				// 크기 회전 위치 순서대로 적용시켜야 합니다.
 
 				WorldPoint *= Scale;
 				WorldPoint = WorldPoint.VectorRotationToDegX(Rotation.X);
 				WorldPoint = WorldPoint.VectorRotationToDegY(Rotation.Y);
 				WorldPoint = WorldPoint.VectorRotationToDegZ(Rotation.Z);
 				WorldPoint += Position;
+
+				// WorldPoint.X = (WorldPoint.X * Scale.X) + Position.Y;
+
+				Trifloat4[VertexCount] = WorldPoint;
 				Tri[VertexCount] = WorldPoint.WindowPOINT();
+			}
+
+			float4 Dir0 = Trifloat4[0] - Trifloat4[1];
+			float4 Dir1 = Trifloat4[1] - Trifloat4[2];
+			float4 Check = float4::Cross3D(Dir1, Dir0);
+
+			if (Check.Z < 1.0f)
+			{
+				continue;
 			}
 			Polygon(DC, &Tri[0], Tri.size());
 		}
