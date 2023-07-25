c6fc983d4237de22ff40ed6b571c06f36c2034cc
ddeoyoung <ksysnake3690@gmail.com>
2023-07-25 13:03:13 +0900

    GameEngineMath 업데이트

--- DirectX_KSY/Content_Cuphead/ContentsCore.cpp
+++ DirectX_KSY/Content_Cuphead/ContentsCore.cpp
@@ -11,6 +11,24 @@ ContentsCore::~ContentsCore()
 
 void ContentsCore::Start()
 {
+	// 회전방향을 외적으로 알아내는 방법
+	float4 PlayerPos = { 3, 5 };
+	float4 MonsterPos = { 5, 5 };
+
+	float4 PlayerDir = { 0, 1, 0 };
+	float4 PlayerLook = (PlayerPos - MonsterPos).NormalizeReturn();
+
+
+	float4 Check = float4::Cross3D(PlayerDir, PlayerLook);
+
+	// float4 Rev = float4::Cross3D({0, 10, 0}, { 0.01f, -10.0f, 0 });
+
+	if (0 > Check.Z)
+	{
+		// 왼쪽으로 돌아라.
+	}
+
+	int a = 0;
 
 }
 
@@ -30,21 +48,28 @@ void ContentsCore::Update(float _Delta)
 		static float4 Scale = { 100.0f, 100.0f, 100.0f }; // 크기
 		static float4 Rotation = { 0, 0, 0 }; // 회전
 		static float4 Position = { 100.0f, 100.0f, 100.0f }; // 이동
-
-		//static float Dir = 1.0f;
-
-		//Scale -= float4(100.0f, 100.0f, 100.0f) * _Delta * Dir;
-
-		//if (100.0f <= abs(Scale.X))
-		//{
-		//	Dir *= -1.0f;
-		//}
-
-
 		Rotation.X += 360.0f * _Delta;
 		Rotation.Y += 360.0f * _Delta;
 		Rotation.Z += 360.0f * _Delta;
 
+		float4x4 Scale4x4;
+		float4x4 Rotation4x4X;
+		float4x4 Rotation4x4Y;
+		float4x4 Rotation4x4Z;
+		float4x4 Rotation4x4;
+		float4x4 Position4x4;
+
+
+		Scale4x4.Scale({ 100, 100, 100 });
+
+		//Rotation4x4X.RotationXDegs(Rotation.X);
+		//Rotation4x4Y.RotationYDegs(Rotation.Y);
+		//Rotation4x4Z.RotationZDegs(Rotation.Z);
+		//Rotation4x4 = Rotation4x4X * Rotation4x4Y * Rotation4x4Z;
+		//Position4x4.Pos({ 100, 100, 100 });
+
+		// 행렬의 곱샘은 교환법칙이 성립하지 않습니다.
+		float4x4 World4x4 = Scale4x4 * Rotation4x4 * Position4x4;
 
 		// 로컬과 월드의 차이입니다.
 		// 사각형을 만들기 위해서 점을 4개 만들었습니다.
@@ -55,40 +80,47 @@ void ContentsCore::Update(float _Delta)
 		std::vector<float4> Vertex;
 		Vertex.resize(4 * 6);
 
+		float4 BaseVertexs[4];
+
+		BaseVertexs[0] = { -0.5f, -0.5f, -0.5f };
+		BaseVertexs[1] = { 0.5f, -0.5f, -0.5f };
+		BaseVertexs[2] = { 0.5f, 0.5f, -0.5f };
+		BaseVertexs[3] = { -0.5f, 0.5f, -0.5f };
+
 		// 앞면
-		Vertex[0] = { -0.5f, -0.5f, -0.5f };
-		Vertex[1] = { 0.5f, -0.5f, -0.5f };
-		Vertex[2] = { 0.5f, 0.5f, -0.5f };
-		Vertex[3] = { -0.5f, 0.5f, -0.5f };
+		Vertex[0] = BaseVertexs[0];
+		Vertex[1] = BaseVertexs[1];
+		Vertex[2] = BaseVertexs[2];
+		Vertex[3] = BaseVertexs[3];
 
 		// 뒷면
-		Vertex[4] = float4::VectorRotationToDegX(Vertex[0], 180.0f);
-		Vertex[5] = float4::VectorRotationToDegX(Vertex[1], 180.0f);
-		Vertex[6] = float4::VectorRotationToDegX(Vertex[2], 180.0f);
-		Vertex[7] = float4::VectorRotationToDegX(Vertex[3], 180.0f);
+		Vertex[4] = float4::VectorRotationToDegX(BaseVertexs[0], 180.0f);
+		Vertex[5] = float4::VectorRotationToDegX(BaseVertexs[1], 180.0f);
+		Vertex[6] = float4::VectorRotationToDegX(BaseVertexs[2], 180.0f);
+		Vertex[7] = float4::VectorRotationToDegX(BaseVertexs[3], 180.0f);
 
 		// 왼쪽이나 오른쪽
-		Vertex[8] = float4::VectorRotationToDegY(Vertex[0], 90.0f);
-		Vertex[9] = float4::VectorRotationToDegY(Vertex[1], 90.0f);
-		Vertex[10] = float4::VectorRotationToDegY(Vertex[2], 90.0f);
-		Vertex[11] = float4::VectorRotationToDegY(Vertex[3], 90.0f);
+		Vertex[8] = float4::VectorRotationToDegY(BaseVertexs[0], 90.0f);
+		Vertex[9] = float4::VectorRotationToDegY(BaseVertexs[1], 90.0f);
+		Vertex[10] = float4::VectorRotationToDegY(BaseVertexs[2], 90.0f);
+		Vertex[11] = float4::VectorRotationToDegY(BaseVertexs[3], 90.0f);
 
 		// 왼쪽이나 오른쪽
-		Vertex[12] = float4::VectorRotationToDegY(Vertex[0], -90.0f);
-		Vertex[13] = float4::VectorRotationToDegY(Vertex[1], -90.0f);
-		Vertex[14] = float4::VectorRotationToDegY(Vertex[2], -90.0f);
-		Vertex[15] = float4::VectorRotationToDegY(Vertex[3], -90.0f);
+		Vertex[12] = float4::VectorRotationToDegY(BaseVertexs[0], -90.0f);
+		Vertex[13] = float4::VectorRotationToDegY(BaseVertexs[1], -90.0f);
+		Vertex[14] = float4::VectorRotationToDegY(BaseVertexs[2], -90.0f);
+		Vertex[15] = float4::VectorRotationToDegY(BaseVertexs[3], -90.0f);
 
 		// 위거나 아래
-		Vertex[16] = float4::VectorRotationToDegX(Vertex[0], 90.0f);
-		Vertex[17] = float4::VectorRotationToDegX(Vertex[1], 90.0f);
-		Vertex[18] = float4::VectorRotationToDegX(Vertex[2], 90.0f);
-		Vertex[19] = float4::VectorRotationToDegX(Vertex[3], 90.0f);
+		Vertex[16] = float4::VectorRotationToDegX(BaseVertexs[0], 90.0f);
+		Vertex[17] = float4::VectorRotationToDegX(BaseVertexs[1], 90.0f);
+		Vertex[18] = float4::VectorRotationToDegX(BaseVertexs[2], 90.0f);
+		Vertex[19] = float4::VectorRotationToDegX(BaseVertexs[3], 90.0f);
 
-		Vertex[20] = float4::VectorRotationToDegX(Vertex[0], -90.0f);
-		Vertex[21] = float4::VectorRotationToDegX(Vertex[1], -90.0f);
-		Vertex[22] = float4::VectorRotationToDegX(Vertex[2], -90.0f);
-		Vertex[23] = float4::VectorRotationToDegX(Vertex[3], -90.0f);
+		Vertex[20] = float4::VectorRotationToDegX(BaseVertexs[0], -90.0f);
+		Vertex[21] = float4::VectorRotationToDegX(BaseVertexs[1], -90.0f);
+		Vertex[22] = float4::VectorRotationToDegX(BaseVertexs[2], -90.0f);
+		Vertex[23] = float4::VectorRotationToDegX(BaseVertexs[3], -90.0f);
 
 
 
@@ -129,6 +161,9 @@ void ContentsCore::Update(float _Delta)
 		// 
 		// short Arr[2][3] = {{0, 1, 2}, {0, 2, 3}}; 24
 
+		// godf
+
+
 		for (size_t indexCount = 0; indexCount < Index.size() / 3; indexCount++)
 		{
 			int ArrIndex[3];
@@ -141,31 +176,25 @@ void ContentsCore::Update(float _Delta)
 			Tri.resize(3);
 			for (size_t VertexCount = 0; VertexCount < Tri.size(); VertexCount++)
 			{
+				// 위치를 더해줌으로해서 월드 상태로 이전시켰다고 한다.
 				float4 WorldPoint = Vertex[ArrIndex[VertexCount]];
 
-				WorldPoint *= Scale;
-				WorldPoint = WorldPoint.VectorRotationToDegX(Rotation.X);
-				WorldPoint = WorldPoint.VectorRotationToDegY(Rotation.Y);
-				WorldPoint = WorldPoint.VectorRotationToDegZ(Rotation.Z);
-				WorldPoint += Position;
-
-				// WorldPoint.X = (WorldPoint.X * Scale.X) + Position.Y;
+				//변환식은 이제 딱 한가지 인것.
+				WorldPoint = WorldPoint * World4x4;
 
 				Trifloat4[VertexCount] = WorldPoint;
 				Tri[VertexCount] = WorldPoint.WindowPOINT();
 			}
-
 			float4 Dir0 = Trifloat4[0] - Trifloat4[1];
 			float4 Dir1 = Trifloat4[1] - Trifloat4[2];
 			float4 Check = float4::Cross3D(Dir1, Dir0);
-
 			if (Check.Z < 1.0f)
 			{
 				continue;
 			}
 			Polygon(DC, &Tri[0], Tri.size());
 		}
-		
+
 
 		GameEngineCore::MainWindow.DoubleBuffering();
 	}
--- DirectX_KSY/GameEngineBase/GameEngineMath.h
+++ DirectX_KSY/GameEngineBase/GameEngineMath.h
@@ -2,6 +2,11 @@
 #include "GameEngineDebug.h"
 #include <Windows.h>
 
+#include <d3d11_4.h>
+#include <d3dcompiler.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
 // 설명 :
 class GameEngineMath
 {
@@ -266,19 +271,7 @@ public:
 		return VectorRotationToRadX(_Value, _Deg * GameEngineMath::D2R);
 	}
 
-	static float4 VectorRotationToRadX(const float4& _Value, const float _Rad)
-	{
-		//Rot.X = _Value.X * cosf(_Rad) - _Value.Y * sinf(_Rad);
-		//Rot.Y = _Value.X * sinf(_Rad) + _Value.Y * cosf(_Rad);
-
-
-		// 왜 이 공식인지를 이해해야 합니다.
-		float4 Rot;
-		Rot.X = _Value.X;
-		Rot.Y = _Value.Z * sinf(_Rad) + _Value.Y * cosf(_Rad);
-		Rot.Z = _Value.Z * cosf(_Rad) - _Value.Y * sinf(_Rad);
-		return Rot;
-	}
+	static float4 VectorRotationToRadX(const float4& _Value, const float _Rad);
 
 	float4 VectorRotationToDegY(const float _Deg)
 	{
@@ -291,15 +284,7 @@ public:
 		return VectorRotationToRadY(_Value, _Deg * GameEngineMath::D2R);
 	}
 
-	static float4 VectorRotationToRadY(const float4& _Value, const float _Rad)
-	{
-		// 왜 이 공식인지를 이해해야 합니다.
-		float4 Rot;
-		Rot.X = _Value.X * cosf(_Rad) - _Value.Z * sinf(_Rad);
-		Rot.Y = _Value.Y;
-		Rot.Z = _Value.X * sinf(_Rad) + _Value.Z * cosf(_Rad);
-		return Rot;
-	}
+	static float4 VectorRotationToRadY(const float4& _Value, const float _Rad);
 
 	float4 VectorRotationToDegZ(const float _Deg)
 	{
@@ -311,15 +296,7 @@ public:
 		return VectorRotationToRadZ(_Value, _Deg * GameEngineMath::D2R);
 	}
 
-	static float4 VectorRotationToRadZ(const float4& _Value, const float _Rad)
-	{
-		// 왜 이 공식인지를 이해해야 합니다.
-		float4 Rot;
-		Rot.X = _Value.X * cosf(_Rad) - _Value.Y * sinf(_Rad);
-		Rot.Y = _Value.X * sinf(_Rad) + _Value.Y * cosf(_Rad);
-		Rot.Z = _Value.Z;
-		return Rot;
-	}
+	static float4 VectorRotationToRadZ(const float4& _Value, const float _Rad);
 
 
 	//                                       90.0f
@@ -354,7 +331,7 @@ public:
 		return GetUnitVectorFromRad(_Degree * GameEngineMath::D2R);
 	}
 
-	float4 operator*(const class float4x4& _Other);
+	float4 operator*(const class float4x4& _Other) const;
 };
 
 class GameEngineRect
@@ -502,8 +479,78 @@ public:
 	}
 
 
+	void RotationXDegs(const float _Value)
+	{
+		RotationXRad(_Value * GameEngineMath::D2R);
+	}
+
+	void RotationXRad(const float _Value)
+	{
+		Identity();
+		// DirectX::XMMatrixRotationX
+		float CosValue = cosf(_Value);
+		float SinValue = sinf(_Value);
+		Arr2D[1][1] = CosValue;
+		Arr2D[1][2] = SinValue;
+		Arr2D[2][1] = -SinValue;
+		Arr2D[2][2] = CosValue;
+	}
+
+	void RotationYDegs(const float _Value)
+	{
+		RotationYRad(_Value * GameEngineMath::D2R);
+	}
+
+	void RotationYRad(const float _Value)
+	{
+		Identity();
 
 
+		// DirectX::XMMatrixRotationY
+		float CosValue = cosf(_Value);
+		float SinValue = sinf(_Value);
+		Arr2D[0][0] = CosValue;
+		Arr2D[0][2] = -SinValue;
+		Arr2D[2][0] = SinValue;
+		Arr2D[2][2] = CosValue;
+	}
+
+	void RotationZDegs(const float _Value)
+	{
+		RotationZRad(_Value * GameEngineMath::D2R);
+	}
+
+	void RotationZRad(const float _Value)
+	{
+		Identity();
+
+
+		// DirectX::XMMatrixRotationZ
+		float CosValue = cosf(_Value);
+		float SinValue = sinf(_Value);
+		Arr2D[0][0] = CosValue;
+		Arr2D[0][1] = SinValue;
+		Arr2D[1][0] = -SinValue;
+		Arr2D[1][1] = CosValue;
+
+
+		//					    [cosf(_Rad)][sinf(_Rad)][02][03]
+		//					    [-sinf(_Rad)][cosf(_Rad)][12][13]
+		//					    [20][21][22][23]
+		//					    [30][31][32][33]
+		// [x][y][z][w]        = rx  ry  rz  rw
+
+		// [x]*[00] + [y] *[10] + [z] * [20] + [w] * [30]
+
+		//float4 Rot * 행렬;
+
+		//Rot.X = _Value.X * cosf(_Rad) - _Value.Y * sinf(_Rad);
+		//Rot.Y = _Value.X * sinf(_Rad) + _Value.Y * cosf(_Rad);
+		//Rot.Z = _Value.Z;
+
+
+		// 회전을 시킬수 있는 행렬이 되어야 한다.
+	}
 
 	float4x4 operator*(const float4x4& _Other)
 	{
