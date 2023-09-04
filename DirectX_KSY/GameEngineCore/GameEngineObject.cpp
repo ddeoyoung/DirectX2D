3c8be7f6a548f26d93d4a8ecd35f67a9d30fcc06
ddeoyoung <ksysnake3690@gmail.com>
2023-09-04 16:49:41 +0900

    엔진 업데이트 : Release 구조

--- DirectX_KSY/GameEngineCore/GameEngineObject.cpp
+++ DirectX_KSY/GameEngineCore/GameEngineObject.cpp
@@ -1,11 +1,11 @@
 #include "PreCompile.h"
 #include "GameEngineObject.h"
 
-GameEngineObject::GameEngineObject() 
+GameEngineObject::GameEngineObject()
 {
 }
 
-GameEngineObject::~GameEngineObject() 
+GameEngineObject::~GameEngineObject()
 {
 }
 
@@ -47,6 +47,36 @@ void GameEngineObject::AllLevelEnd(class GameEngineLevel* _NextLevel)
 			_Child->LevelEnd(_NextLevel);
 		}
 	}
+}
+
+void GameEngineObject::AllReleaseCheck()
+{
+	if (true == IsDeath())
+	{
+		Release();
+	}
+
+	// 들고있는 녀석들은 전부다 액터겠지만
+	for (std::pair<const int, std::list<std::shared_ptr<GameEngineObject>>>& _Pair : Childs)
+	{
+		std::list<std::shared_ptr<GameEngineObject>>& Group = _Pair.second;
+
+		std::list<std::shared_ptr<GameEngineObject>>::iterator Start = Group.begin();
+		std::list<std::shared_ptr<GameEngineObject>>::iterator End = Group.end();
+
+		for (; Start != End;)
+		{
+			if (false == (*Start)->IsDeath())
+			{
+				(*Start)->AllReleaseCheck();
+				++Start;
+				continue;
+			}
+
+			Start = Group.erase(Start);
+		}
+	}
+
 }
 
 void GameEngineObject::AllUpdate(float _Delta)
