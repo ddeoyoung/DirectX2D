7da5f6256e513716bd1b96baeb7cf6c0570d0708
ddeoyoung <ksysnake3690@gmail.com>
2023-09-23 17:32:23 +0900

    칩스베티건 FSM 기본 추가

--- DirectX_KSY/Content_Cuphead/ChipsBettigan.cpp
+++ DirectX_KSY/Content_Cuphead/ChipsBettigan.cpp
@@ -9,3 +9,48 @@ ChipsBettigan::~ChipsBettigan()
 {
 }
 
+void ChipsBettigan::Start()
+{
+	// Create Sprite Folder
+	if (nullptr == GameEngineSprite::Find("Chips_Idle"))
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParentToExistsChild("GameEngineResources");
+		Dir.MoveChild("ContentsResources\\Texture\\Stage\\ChipsBettigan\\Boss");
+
+		std::vector<GameEngineDirectory> Directorys = Dir.GetAllDirectory();
+
+		for (size_t i = 0; i < Directorys.size(); i++)
+		{
+			GameEngineDirectory& Dir = Directorys[i];
+			GameEngineSprite::CreateFolder(Dir.GetStringPath());
+		}
+	}
+
+	// Create Animation
+	MainRenderer = CreateComponent<GameEngineSpriteRenderer>();
+
+	MainRenderer->CreateAnimation("Chips_Intro", "Chips_Intro");
+	MainRenderer->CreateAnimation("Chips_Idle", "Chips_Idle", 0.05f);
+	MainRenderer->ChangeAnimation("Chips_Idle");
+
+	MainRenderer->AutoSpriteSizeOn();
+	MainRenderer->SetPivotType(PivotType::Bottom);
+
+	Transform.SetLocalPosition({ 1000 , -650 });
+}
+
+void ChipsBettigan::Update(float _Delta)
+{
+
+}
+
+void ChipsBettigan::ChangeState(ChipsState _State)
+{
+
+}
+
+void ChipsBettigan::StateUpdate(float _Delta)
+{
+
+}
--- DirectX_KSY/Content_Cuphead/ChipsBettigan.h
+++ DirectX_KSY/Content_Cuphead/ChipsBettigan.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <GameEngineCore\GameEngineActor.h>
 
+enum class ChipsState
+{
+	None,
+	Intro,
+	Idle,
+	Spin,
+	Death,
+};
+
 // Ό³Έν :
 class ChipsBettigan : public GameEngineActor
 {
@@ -16,6 +25,18 @@ public:
 	ChipsBettigan& operator=(ChipsBettigan&& _Other) noexcept = delete;
 
 protected:
+	void ChangeState(ChipsState _State);
+	void StateUpdate(float _Delta);
+	void ChangeAnimationState(const std::string& _StateName);
+
+protected:
+	void Start() override;
+	void Update(float _Delta) override;
+
+	std::string CurState = "";
+	ChipsState State = ChipsState::None;
+
+	std::shared_ptr<class GameEngineSpriteRenderer> MainRenderer = nullptr;
 
 private:
 
