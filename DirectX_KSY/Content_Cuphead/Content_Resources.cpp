8db706a842dca9de580b7b64f37cb4af7c90fb22
ddeoyoung <ksysnake3690@gmail.com>
2023-10-17 11:26:18 +0900

    코드 정리

---
+++ DirectX_KSY/Content_Cuphead/Content_Resources.cpp
@@ -0,0 +1,32 @@
+#include "PreCompile.h"
+#include "ContentsCore.h"
+
+void ContentsCore::ContentRes()
+{
+	GameEngineRenderTarget::IsDepth = false;
+
+	// OverRay
+	{
+		D3D11_BLEND_DESC Desc = {};
+		Desc.IndependentBlendEnable = false;
+		Desc.RenderTarget[0].BlendEnable = true;
+		Desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
+
+		Desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
+		Desc.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
+		Desc.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
+
+		Desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
+		Desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ZERO;
+		Desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
+
+		std::shared_ptr<GameEngineBlend> Blend = GameEngineBlend::Create("OverRay", Desc);
+	}
+
+	{
+		std::shared_ptr<GameEngineMaterial> Mat = GameEngineMaterial::Create("2DTextureOver");
+		Mat->SetVertexShader("TextureShader_VS");
+		Mat->SetPixelShader("TextureShader_PS");
+		Mat->SetBlendState("OverRay");
+	}
+}
--- DirectX_KSY/Content_Cuphead/ContentsCore.cpp
+++ DirectX_KSY/Content_Cuphead/ContentsCore.cpp
@@ -10,7 +10,6 @@
 #include "ChipsBettiganLevel.h"
 #include "OutroLevel.h"
 
-#include <GameEngineCore/GameEngineBlend.h>
 
 ContentsCore::ContentsCore()
 {
@@ -34,7 +33,7 @@ void ContentsCore::Start()
 	GameEngineCore::CreateLevel<ChipsBettiganLevel>("ChipsBettiganLevel");
 	GameEngineCore::CreateLevel<OutroLevel>("OutroLevel");
 
-	GameEngineCore::ChangeLevel("TutorialLevel");
+	GameEngineCore::ChangeLevel("KingDiceLevel");
 }
 
 void ContentsCore::Update(float _Delta)
@@ -45,34 +44,4 @@ void ContentsCore::Update(float _Delta)
 void ContentsCore::Release()
 {
 
-}
-
-void ContentsCore::ContentRes()
-{
-	GameEngineRenderTarget::IsDepth = false;
-
-	// OverRay
-	{
-		D3D11_BLEND_DESC Desc = {};
-		Desc.IndependentBlendEnable = false;
-		Desc.RenderTarget[0].BlendEnable = true;
-		Desc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
-
-		Desc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
-		Desc.RenderTarget[0].SrcBlend = D3D11_BLEND_ONE;
-		Desc.RenderTarget[0].DestBlend = D3D11_BLEND_ONE;
-
-		Desc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
-		Desc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_ZERO;
-		Desc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ONE;
-
-		std::shared_ptr<GameEngineBlend> Blend = GameEngineBlend::Create("OverRay", Desc);
-	}
-
-	{
-		std::shared_ptr<GameEngineMaterial> Mat = GameEngineMaterial::Create("2DTextureOver");
-		Mat->SetVertexShader("TextureShader_VS");
-		Mat->SetPixelShader("TextureShader_PS");
-		Mat->SetBlendState("OverRay");
-	}
 }
