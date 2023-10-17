16122a9b41fe4ec82793a1f2dfe01b32f6e6ab89
ddeoyoung <ksysnake3690@gmail.com>
2023-10-17 17:36:07 +0900

    GUI Level Change 버튼 추가

--- DirectX_KSY/Content_Cuphead/ContentGUI.cpp
+++ DirectX_KSY/Content_Cuphead/ContentGUI.cpp
@@ -17,13 +17,31 @@ void ContentGUI::Start()
 void ContentGUI::OnGUI(GameEngineLevel* _Level, float _DeltaTime)
 {
 	// Mouse Pos
+	ImGui::Text("Mouse Pos");
 	std::string MousePos = GameEngineCore::MainWindow.GetMousePos().ToString();
 	ImGui::Text(MousePos.c_str());
+	ImGui::Text("\n");
 
-	// Collision On/OFf
+
+	// Collision On/Off
+	ImGui::Text("Collision On/Off");
 	if (ImGui::Button("Collision On/Off"))
 	{
 		GameEngineLevel::IsDebug = !GameEngineLevel::IsDebug;
+	}
+	ImGui::Text("\n");
+
+
+	// Level Change
+	ImGui::Text("Level Change");
+	if (ImGui::Button("Tutorial Level"))
+	{
+		GameEngineCore::ChangeLevel("TutorialLevel");
+	}
+
+	if (ImGui::Button("KingDice Level"))
+	{
+		GameEngineCore::ChangeLevel("KingDiceLevel");
 	}
 
 }
