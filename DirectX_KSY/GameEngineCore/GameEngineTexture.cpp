9374f5b5547a8a643892ee5f5bbbdbc3fd61824d
ddeoyoung <ksysnake3690@gmail.com>
2023-10-19 00:17:46 +0900

    엔진 업데이트 : Color 색 추가 및 알파 기능

--- DirectX_KSY/GameEngineCore/GameEngineTexture.cpp
+++ DirectX_KSY/GameEngineCore/GameEngineTexture.cpp
@@ -9,6 +9,13 @@
 #endif
 
 const GameEngineColor GameEngineColor::RED = { 255, 0, 0, 255 };
+const GameEngineColor GameEngineColor::GREEN = { 0, 255, 0, 255 };
+const GameEngineColor GameEngineColor::BLUE = { 0, 0, 255, 255 };
+const GameEngineColor GameEngineColor::MAGENTA = { 255, 0, 255, 255 };
+const GameEngineColor GameEngineColor::YELLOW = { 255, 255, 0, 255 };
+const GameEngineColor GameEngineColor::AQUA = { 0, 255, 255, 255 };
+const GameEngineColor GameEngineColor::BLACK = { 0, 0, 0, 255 };
+const GameEngineColor GameEngineColor::WHITE = { 255, 255, 255, 255 };
 
 GameEngineTexture::GameEngineTexture()
 {
@@ -16,6 +23,12 @@ GameEngineTexture::GameEngineTexture()
 
 GameEngineTexture::~GameEngineTexture()
 {
+	if (nullptr != DSV)
+	{
+		DSV->Release();
+		DSV = nullptr;
+	}
+
 	if (nullptr != SRV)
 	{
 		SRV->Release();
