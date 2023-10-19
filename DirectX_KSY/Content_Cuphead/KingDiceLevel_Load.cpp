3a37d3c48a723a6367ca83d5d02e5e08a054e9d3
ddeoyoung <ksysnake3690@gmail.com>
2023-10-19 16:36:08 +0900

    킹다이스 룰렛 Odds, Spaces 추가

--- DirectX_KSY/Content_Cuphead/KingDiceLevel_Load.cpp
+++ DirectX_KSY/Content_Cuphead/KingDiceLevel_Load.cpp
@@ -63,6 +63,85 @@ void KingDiceLevel::LevelStartTextureLoad()
 		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_heart_09.png");
 		GameEngineSprite::CreateSingle("kd_gb_heart_09.png");
 	}
+
+	// KingDice Roulette Odds
+	if (nullptr == GameEngineSprite::Find("kd_gb_main_odds_01.png"))
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParentToExistsChild("GameEngineResources");
+		Dir.MoveChild("ContentsResources\\Texture\\Stage\\KingDice\\StageObject\\Odds\\");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_01.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_01.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_02.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_02.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_03.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_03.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_04.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_04.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_05.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_05.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_06.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_06.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_07.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_07.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_08.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_08.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_main_odds_09.png");
+		GameEngineSprite::CreateSingle("kd_gb_main_odds_09.png");
+	}
+
+	// KingDice Roulette Spaces
+	if (nullptr == GameEngineSprite::Find("kd_gb_space_01.png"))
+	{
+		GameEngineDirectory Dir;
+		Dir.MoveParentToExistsChild("GameEngineResources");
+		Dir.MoveChild("ContentsResources\\Texture\\Stage\\KingDice\\StageObject\\Spaces\\");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_01.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_01.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_02.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_02.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_03.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_03.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_04.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_04.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_05.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_05.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_06.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_06.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_07.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_07.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_08.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_08.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_09.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_09.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_fin.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_fin.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_fin_complete.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_fin_complete.png");
+
+		GameEngineTexture::Load(Dir.GetStringPath() + "kd_gb_space_start_over.png");
+		GameEngineSprite::CreateSingle("kd_gb_space_start_over.png");
+	}
 }
 
 void KingDiceLevel::LevelStartTextureSet()
@@ -89,4 +168,62 @@ void KingDiceLevel::LevelStartTextureSet()
 	std::shared_ptr<ContentActor> Heart9 = CreateActor<ContentActor>();
 	Heart9->SpriteInit("kd_gb_heart_09.png", { 905, -415 });
 
+
+	// Roulette - Odd
+	std::shared_ptr<ContentActor> Odd2 = CreateActor<ContentActor>();
+	Odd2->SpriteInit("kd_gb_main_odds_02.png", { 387, -374 });
+
+	std::shared_ptr<ContentActor> Odd3 = CreateActor<ContentActor>();
+	Odd3->SpriteInit("kd_gb_main_odds_03.png", { 434, -414 });
+
+	std::shared_ptr<ContentActor> Odd4 = CreateActor<ContentActor>();
+	Odd4->SpriteInit("kd_gb_main_odds_04.png", { 552, -466 });
+
+	std::shared_ptr<ContentActor> Odd5 = CreateActor<ContentActor>();
+	Odd5->SpriteInit("kd_gb_main_odds_05.png", { 614, -479 });
+
+	std::shared_ptr<ContentActor> Odd7 = CreateActor<ContentActor>();
+	Odd7->SpriteInit("kd_gb_main_odds_07.png", { 802, -463 });
+
+	std::shared_ptr<ContentActor> Odd8 = CreateActor<ContentActor>();
+	Odd8->SpriteInit("kd_gb_main_odds_08.png", { 855, -443 });
+
+
+	// KingDice Roulette Spaces
+	std::shared_ptr<ContentActor> Space1 = CreateActor<ContentActor>();
+	Space1->SpriteInit("kd_gb_space_01.png", { 300, -352 });
+
+	std::shared_ptr<ContentActor> Space2 = CreateActor<ContentActor>();
+	Space2->SpriteInit("kd_gb_space_02.png", { 342, -408 });
+
+	std::shared_ptr<ContentActor> Space3 = CreateActor<ContentActor>();
+	Space3->SpriteInit("kd_gb_space_03.png", { 400, -456 });
+
+	std::shared_ptr<ContentActor> Space4 = CreateActor<ContentActor>();
+	Space4->SpriteInit("kd_gb_space_04.png", { 533, -517 });
+
+	std::shared_ptr<ContentActor> Space5 = CreateActor<ContentActor>();
+	Space5->SpriteInit("kd_gb_space_05.png", { 607, -530 });
+
+	std::shared_ptr<ContentActor> Space6 = CreateActor<ContentActor>();
+	Space6->SpriteInit("kd_gb_space_06.png", { 678, -535 });
+
+	std::shared_ptr<ContentActor> Space7 = CreateActor<ContentActor>();
+	Space7->SpriteInit("kd_gb_space_07.png", { 819, -511 });
+
+	std::shared_ptr<ContentActor> Space8 = CreateActor<ContentActor>();
+	Space8->SpriteInit("kd_gb_space_08.png", { 882, -487 });
+
+	std::shared_ptr<ContentActor> Space9 = CreateActor<ContentActor>();
+	Space9->SpriteInit("kd_gb_space_09.png", { 940, -450 });
+
+	std::shared_ptr<ContentActor> SpaceStartOver = CreateActor<ContentActor>();
+	SpaceStartOver->SpriteInit("kd_gb_space_start_over.png", { 1025, -353 });
+
+	std::shared_ptr<ContentActor> SpaceFinish = CreateActor<ContentActor>();
+	SpaceFinish->SpriteInit("kd_gb_space_fin.png", { 1055, -295 });
+
+	std::shared_ptr<ContentActor> SpaceFinishComplete = CreateActor<ContentActor>();
+	SpaceFinishComplete->SpriteInit("kd_gb_space_fin_complete.png", { 1055, -295 });
+	SpaceFinishComplete->Off();
 }
